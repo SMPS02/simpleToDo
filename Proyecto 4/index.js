c5a9c7c8ec605bdcb292befad7ed606d96ed705c
SMPS02 <simonmiguel1829@gmail.com>
2025-07-18 00:25:01 +0200

    Trabajo casi terminado

--- Proyecto 4/index.js
+++ Proyecto 4/index.js
@@ -1,35 +1,55 @@
-//---------------------------------------------------------------
-//VARIABLES GLOBALES
-const taskContainer = document.getElementById("tasksContainer")
-const taskCollector = document.getElementById("getTask")
-const inputTask = document.getElementById("writeTask");
-const container = document.getElementById("tasksContainerDiv")
-const deleteLocalStorage = document.getElementById("deleteLocalStorage");
+//---------------------------------------------------------------------------------------
+//VARIABLES DESDE EL DOM
 
-//Menu desplegable
-const buttonMenu = document.getElementById("buttonMenu");
-const menu = document.getElementById("menu")
+//variable que representa a los elementos almancenados en el LocalStorage
+const variablesDeLocal = obtenerDeLocal();
 
- const buttonClose = document.getElementById("closeNav");
- const buttonOpen = document.getElementById("openNav");
+//Array de objetos (Las tareas)
+let tareas = variablesDeLocal || [];
+
+//ul que contiene a las tareas
+const ul = document.getElementById("tasksContainer");
+
+//Input  donde se ingresan las tareas
+const input = document.getElementById("writeTask");
+
+//Contenedor de las tareas
+const contenedor = document.getElementById("main");
+
+//FUCNIONES
+
+//Funcion de buena pratica para agregar elementos al local Storage (Reducir codigo, más legible, etc)
+function agregarAlLocal(){
+
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+//Funcion de buena practica para obtener elementos al local Storage (Reducir codigo, más legible, etc)
+function obtenerDeLocal(){
 
 
-//FUNCIONES
-function taskCreator (task){
+    return JSON.parse(localStorage.getItem("tareas")) || [];
 
-    
-    taskContainer.innerHTML += 
-    `<li class="listItem">
+}
+
+//Funcion para crear las tareas y dibujarlas en el HTML.
+function agregarAlHTML ({tarea, id, done, desing}){
+
+    ul.innerHTML += 
+    `<li class="listItem" id = "${id}">
               
                 <div class="normalSpace">
+                
+                <div class="taskAndButtons">
                     <div class="taskSpace">
-                    <h2 class="task">
-                        ${task}
-                    </h2>
+                    <h1 class="task ${desing ? "completed" : ""}">
+                        ${tarea}
+                    </h1>
                     </div> 
+
                     <div class="doneOrNot">
                         <button class="notDone" id="done">
-                            Not Done
+                            ${done ? "Done" : "Not Done"}
                         </button>
 
                         <button class="editTask" id="edit">
@@ -42,283 +62,169 @@ function taskCreator (task){
                     </div>
                     </div>
 
+                    <div class="editContainer hidden-2">
+                        <input type="text" class="taskEditor">
+                        <button class="gettingtaskEdited">
+                         Editar
+                        </button>
+                    </div>
+
                 </li>`
+   
+}
+
+function borrarTarea(e){
+   const liID = e.target.parentElement.parentElement.parentElement.parentElement.id;
+   
+   const li = document.getElementById(liID);
+  
+   tareas = tareas.filter((tarea) => tarea.id != liID);
+   
+   ul.innerHTML = "";
+   
+   tareas.forEach(agregarAlHTML);
+   
+   agregarAlLocal();
+}
+
+function mostrarEdicion(e){
+    const liID = e.target.parentElement.parentElement.parentElement.parentElement.id;
+    const li = document.getElementById(liID);
+    const contenedor = li.querySelector(".editContainer");
+    li.classList.toggle("extend");
+    contenedor.classList.toggle("hidden-2");
+
+
 
 }
 
-function isThisKeyHere (key){
+function cargarContenido(e){
+    tareas.forEach(agregarAlHTML);
+}
 
-    for(let i = 0; i<localStorage.length; i++){
+function editarTarea(e){
 
-        if(key === localStorage.key(i)){
-            return true;
+    const elementosLocalStorage = obtenerDeLocal();
+    const liID = e.target.parentElement.parentElement.parentElement.id;
+    const inputEdicion = e.target.previousElementSibling;
+
+    elementosLocalStorage.forEach((tarea) =>{
+        if(tarea.id === liID){
+            tarea.tarea = inputEdicion.value;
         }
-    }
-    return false;
-    
+    })
+    tareas = elementosLocalStorage;
+    agregarAlLocal();
+
+    ul.innerHTML = ""
+
+    tareas.forEach(agregarAlHTML);
+
+    inputEdicion.value = "";
+
 }
-
-function getKey (key){
-
-    for(let i = 0; i<localStorage.length; i++){
-
-        if(key === localStorage.key(i)){
-            return key;
-        }
-    }
-    return "No se ha encontrado esta llave";
-    
-}
+// function tachar(e){
+//      const localVariables = obtenerDeLocal();
+//      const liID = e.target.parentElement.parentElement.parentElement.parentElement.id;
+//      const li = document.getElementById(liID);
+//      localVariables.forEach((tarea) =>{
+//         if(tarea.id === li.id){
+//             li.desing = !li.desing;
+//         }
+//      })
+//      tareas = localVariables;
+//      agregarAlLocal();
+//      ul.innerHTML = ""
+//      tareas.forEach(agregarAlHTML);
+// }
 
 //EVENTOS
-//Event to sweep the buttons done and not done
-container.addEventListener("click", (e) =>{
 
-    const li = e.target.closest("li")
-     //--------------------------------------------------------------------------------       
-    //Crear el elemento
-    const taskEditor = document.createElement("input");
-    taskEditor.className = "taskEditor"
+//Eventos para aplicar las funciones
+document.addEventListener("click", (e)=>{
 
-     const saveButton = document.createElement("button");
-    saveButton.textContent = "Editar"
-    saveButton.className = "gettingtaskEdited"
-    //--------------------------------------------------------------------------------
+    //Evento para agregar las tareas al HTML y al local Storage.
+    if(e.target.classList.contains("getTask")){
 
-
-    //--------------------------------------------------------------------------------
-    //Delete the task
-    if(e.target.classList.contains("deleteTask")){
-        let confirmation = false;
-        let key = prompt("Ingresa la clave de este valor para eliminar");
-        
-        while (confirmation === false){
-            if(isThisKeyHere(key)=== true){
-            localStorage.removeItem(key);
-            li.remove();
-            confirmation = true;
-            }else{
-
-                key = prompt("Ingresa la clave de este valor para eliminar");
-
-            }
-        }
-                
-    }
-    //--------------------------------------------------------------------------------
-
-
-
-    //--------------------------------------------------------------------------------
-    //Sweep through "done" and "not done" buttons
-    if(e.target.classList.contains("notDone")){
-        
-        e.target.classList.toggle("done");
-        e.target.classList.toggle("notDone");
-    }else if(e.target.classList.contains("done")){
-
-        e.target.classList.toggle("done");
-        e.target.classList.toggle("notDone");
-    }
-    if(e.target.classList.contains("notDone")){
-        
-        e.target.textContent = "Not done"
-    }else if(e.target.classList.contains("done")){
-
-        e.target.textContent = "Done"
-    }
-    //--------------------------------------------------------------------------------
-
-
-
-    //--------------------------------------------------------------------------------
-    //Create Editon elements
-    if(e.target.classList.contains("editTask")){
-
-        const eButton = li.querySelector(".gettingtaskEdited")
-        const eInput = li.querySelector(".taskEditor")
-        const task = li.querySelector(".task");
-      
-        if(eButton && eInput){
-            eButton.remove();
-            eInput.remove();
-            li.style.height = ""
+        if(input.value===""){
+            alert("Debes escribir algo");
+            return;
         }else{
 
-            const taskEditor = document.createElement("input");
-            taskEditor.className = "taskEditor"
-            taskEditor.value = task.textContent.trim();
+           const LaTarea = {
+            tarea: input.value,
+            id: crypto.randomUUID(),
+            done: false,
+            desing: false
+           }
 
-            const saveButton = document.createElement("button");
-            saveButton.textContent = "Editar"
-            saveButton.className = "gettingtaskEdited"
+           tareas = [...tareas, LaTarea];
+           agregarAlHTML(LaTarea);
+           agregarAlLocal();
 
-            li.append(taskEditor);
-            li.append(saveButton);
-            li.style.height = "150px"
-            taskEditor.style.height = "100px"
-        
         }
-     
-        
+        input.value = "";
 
-       
     }
-    //--------------------------------------------------------------------------------
-    //Save task and delete Edition Elements
+
+    //Evento para borrar la tarea
+    if(e.target.classList.contains("deleteTask")){
+        borrarTarea(e);
+    }
+
+    //Evento para ver si la tarea está hecha o no hecha.
+    if(e.target.classList.contains("notDone") || e.target.classList.contains("done")){
+        const localVariables = obtenerDeLocal();
+        const liID = e.target.parentElement.parentElement.parentElement.parentElement.id;
+        const li = document.getElementById(liID);
+
+       localVariables.forEach((element) =>{
+        if(element.id === li.id){
+            element.done = !element.done;
+            element.desing = !element.desing;
+        }
+        tareas = localVariables;
+        agregarAlLocal();
+
+        ul.innerHTML = ""
+        tareas.forEach(agregarAlHTML);
+        // tachar(e);
+
+
+       })
+    }
+    //Evento para mostrar el apartado de edición
+    if(e.target.classList.contains("editTask")){
+        mostrarEdicion(e);
+    }
+
+    //Editar tarea
     if(e.target.classList.contains("gettingtaskEdited")){
 
-        let confirmation = false;
-        let key = prompt("Ingrese la llave de esta tarea");
-
-        while(confirmation === false){
-        if(isThisKeyHere(key)===true){
-        const taskText = li.querySelector(".task");
-        const editedTask = li.querySelector(".taskEditor");
-        const saveButton = li.querySelector(".gettingtaskEdited");
-
-        const claveAnterior = prompt("Ingrese la clave de esta tarea");
-        localStorage.removeItem(claveAnterior);
-        const claveNueva = prompt("Ingrese una nueva clave para este nuevo elemento");
-        if(taskText && editedTask.value != ""){
-            taskText.textContent = editedTask.value;
-        }
-        localStorage.setItem(taskText.textContent, claveNueva);
-        saveButton.remove();
-        editedTask.remove();
-        li.style.height = "";
-        confirmation = true;
-        }else{
-            key = prompt("Ingrese la llave de esta tarea");
-            }
-        }
-        
+        editarTarea(e);
     }
-    //--------------------------------------------------------------------------------
-    
-})
-
-//Event to draw the task on the HTML document
-taskCollector.addEventListener("click", (e) => {
-
-    if(inputTask.value === ""){
-
-        alert("You must write someting");
-    }
-    else{
-
-    let confirmation = false;
-    const task = inputTask.value;
-    
-    let clave = prompt("Ingrese una llave para este valor");
-    while(confirmation===false){
-        if(isThisKeyHere(clave) === true){
-            confirmation = false
-            clave = prompt("Esta llave ya está en uso, utilce otra")
-        }
-            confirmation = true
-        
-    }
-    taskCreator(task);
-    localStorage.setItem(clave, JSON.stringify(task));
-    inputTask.value = ""
-    
-}
-
-    
-})
-
-//Save the current information that are on the local Storage
-
-window.addEventListener("DOMContentLoaded", ()=>{
-    for(let i = 0; i<localStorage.length; i++){
-
-        const key = localStorage.key(i);
-        const value = JSON.parse(localStorage.getItem(key));
-        taskCreator(value);
-    }
-})
-//Event to hover the task Key
-container.addEventListener("mouseover", (e)=> {
-
-    const li = e.target.closest("li");
-    if(!li) return;
-    const task = li.querySelector(".task");
-
-    if(e.target.classList.contains("task")){
-
-        //Creation of the div
-       const keyDiv = document.createElement("div");
-       keyDiv.className = "keyDiv";
-
-       //Creation of the div that will be an arrow
-       const arrowDiv = document.createElement("div");
-       arrowDiv.className = "arrowDiv";
-
-       //Save the key in the div
-       keyDiv.textContent = "Proximante...";
-       
-
-       li.append(keyDiv);
-       li.append(arrowDiv);
-    }
-})
-
-//Event to quit the hover 
-container.addEventListener("mouseout", (e)=> {
-
-    const li = e.target.closest("li");
-    if(!li) return;
-    const existKey = li.querySelector(".keyDiv");
-    const existArrow = li.querySelector(".arrowDiv");
-
-    if(e.target.classList.contains("task")){
-       
-        if(existKey && existArrow){
-
-            existKey.remove()
-            existArrow.remove();
-        }
-    }
-})
-deleteLocalStorage.addEventListener("click", (e)=>{
-
-    const  liS = taskContainer.querySelectorAll(".listItem");
-    localStorage.clear();
-    liS.forEach(element=>element.remove());
-})
-
-//Event to show and hide the dynamic menu
-buttonMenu.addEventListener("click", (e)=>{
-
-    
-    if(e.target.classList.contains("openNav")){
-        menu.classList.toggle("hidden")
-        buttonClose.classList.toggle("hidden-2");
-        buttonOpen.classList.toggle("hidden-2");
-    } 
-    
-    if(e.target.classList.contains("closeNav")){
-        menu.classList.toggle("hidden")
-        buttonClose.classList.toggle("hidden-2");
-        buttonOpen.classList.toggle("hidden-2");
+    //Borrar todas las tareas
+    if(e.target.classList.contains("deleteLocalStorage")){
+        localStorage.clear();
+        ul.innerHTML = "";
     }
 
-        
-    
 })
 
-//Cosas para hacer para este proyecto (Se irán borrando mientras se vayan completando)------------------------------
+window.addEventListener("DOMContentLoaded", (e)=>{
+    cargarContenido(e);
+})
 
-// - Arreglar lo de que las tareas se borren cuando se acualice la pagina, aunque estén guardadas en el localStorage
-
-// - Ver el como se podría guardar la variable taskNumber junto al evento de mouseOver para que si se borra la tarea, se borre el numero y se edite y todo lo demás.
-
-// - Completar la parte de los apartados para poder entender la pagina y todo lo demás
-
-// - 
-
-
-
-
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        document.querySelector(".getTask").click();
+    }
+});
+document.addEventListener("keydown", (e) => {
+    if (e.target.classList.contains("taskEditor") && e.key === "Enter") {
+        const editButton = e.target.nextElementSibling;
+        editButton.click();
+    }
+});
+//-------------------------------------------------------------------------------------------
 
